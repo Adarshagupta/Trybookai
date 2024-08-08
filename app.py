dd43da4cef6b718ce9843f9199b949ee28edd4e0
Adarsh Gupta <blueadarsh1@gmail.com>
2024-08-08 10:55:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -272,26 +272,4 @@ def download_saved_pdf(pdf_id):
         return jsonify({'error': str(e)}), 500
 
 if __name__ == "__main__":
-    from gunicorn.app.base import BaseApplication
-
-    class StandaloneApplication(BaseApplication):
-        def __init__(self, app, options=None):
-            self.options = options or {}
-            self.application = app
-            super().__init__()
-
-        def load_config(self):
-            config = {key: value for key, value in self.options.items()
-                      if key in self.cfg.settings and value is not None}
-            for key, value in config.items():
-                self.cfg.set(key.lower(), value)
-
-        def load(self):
-            return self.application
-
-    options = {
-        'bind': '0.0.0.0:5151',
-        'workers': 4,
-        'worker_class': 'aiohttp.GunicornWebWorker'
-    }
-    StandaloneApplication(app, options).run()
+    app.run(host='0.0.0.0', port=5151)
